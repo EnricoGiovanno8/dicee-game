6b07364c2a203442b62156cd4f7b7938808c8e6e
EnricoAccelist <enrico.giovanno@project.accelist.com>
2023-01-09 22:30:59 +0700

    Dicee Game Refactor

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const randomNumber1 = Math.ceil(Math.random() * 5 + 1);
-const randomNumber2 = Math.ceil(Math.random() * 5 + 1);
+const randomNumber1 = Math.floor(Math.random() * 6) + 1;
+const randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 document.querySelector(".img1").setAttribute('src', `./images/dice${randomNumber1}.png`);
 document.querySelector(".img2").setAttribute('src', `./images/dice${randomNumber2}.png`);
